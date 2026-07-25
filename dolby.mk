79e24279e12fff70ed2dec2267fe00d97600aa6b
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-07-25 08:29:17 +0530

    dolby: Update Dolby volume listener stack
    Change-Id: I8a73d615c6c6131427620a25fbf6098833014022
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- dolby.mk
+++ dolby.mk
@@ -150,9 +150,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libdapparamstorage-dolby \
     libdapparamstorage \
-    libdlbpreg \
     libstagefright_foundation-dolby \
-    libdlbvol \
     libswdap \
     libswgamedap \
     libswvqe \
@@ -168,6 +166,12 @@ PRODUCT_PACKAGES += \
     vendor.dolby.hardware.dms@2.0-impl \
     vendor.dolby.hardware.dms@2.0-service \
     vendor.dolby.media.c2@1.0-service
+
+PRODUCT_PACKAGES += \
+    libdlbpreg \
+    libstagefright_foundation-atmos \
+    libdlbvol \
+    vendor.dolby.hardware.dms@2.0-atmos.so
 
 ifeq ($(TARGET_INCLUDES_DolbyVision),true)
 # Dolby vision Proprietary blobs
